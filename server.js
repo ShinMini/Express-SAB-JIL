c4536b9fde6972bc18081af4a6421a193515b828
Shin__Mini <gusals121234@gmail.com>
2022-09-12 11:54:23 +0900

    update

--- server.js
+++ server.js
@@ -2,13 +2,30 @@
 const express = require('express');
 const app = express();
 
-app.use("/", function(req, res){
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", function (req, res) {
      res.sendFile(__dirname + '/index.html');
 });
 
-// 8081 서버는 web socket 방식으로 서버와 통신하도록 설정
+app.use('/static', express.static('src'));
+
+// doGET method
+app.get('/', function (req, res) {
+     res.send('Hello World!');
+});
+
+// doPOST method
+app.post('/', function (req, res) {
+     res.send('Got a POST request');
+});
+
 app.listen(8080);
 
+// web soket 
+// 8081 서버는 web socket 방식으로 서버와 통신하도록 설정
 const WebSocket = require('ws');
 
 const socket = new WebSocket.Server({
@@ -17,15 +34,13 @@ const socket = new WebSocket.Server({
 
 // onclick을 통해 메세지 수신 받은 것 처리하기
 socket.on('connection', (ws, req) => {  // eventListener 부착하는 식으로 코드성작성
-
-     ws.on('message', (msg)=>{
+     ws.on('message', (msg) => {
           console.log('유저가 보낸거: ' + msg); // 유저에게 받은 메세지 서버로 받아옴
-          ws.send('꺼져'); // 웹소켓으로 서버에서 유저로 메세지 전송
+          ws.send('nope!'); // 웹소켓으로 서버에서 유저로 메세지 전송
      })
 });
-     /* ws 대신 socket.io 라이브러리를 쓰면, 
-     - 연결 끊키면 자동 재접속 가능, 
-     - 웹소켓 접속자마다 자동 id 부여, 
-     - 모든 웹소켓 유저에게 전체 메세지 전송가능, 
-     - 웹소켓 방을 생성 가능 등, 다양한 이점이 있다. 
-     */
+     /* ws 대신 socket.io 라이브러리를 쓰면,
+- 연결 끊키면 자동 재접속 가능,꺼져
+- 모든 웹소켓 유저에게 전체 메세지 전송가능, 
+- 웹소켓 방을 생성 가능 등, 다양한 이점이 있다. 
+*/
